bf447eadcbd619d3a95217a67e5e219a778a60d9
namlee1805 <hoainamheo2003@gmail.com>
2024-07-14 17:31:46 +0700

    update login api

--- lib/common/widgets/login_signup/social_buttons.dart
+++ lib/common/widgets/login_signup/social_buttons.dart
@@ -1,13 +1,154 @@
+// import 'package:flutter/material.dart';
+// import 'package:get/get.dart';
+// import 'package:onedaytrip/features/authentication/screens/signup/fill_infor_signup_google.dart';
+//
+// import '../../../utils/constants/colors.dart';
+// import '../../../utils/constants/image_strings.dart';
+// import '../../../utils/constants/sizes.dart';
+//
+// class TSocialButtons extends StatelessWidget {
+//   const TSocialButtons({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Row(
+//       mainAxisAlignment: MainAxisAlignment.center,
+//       children: [
+//         Container(
+//           decoration: BoxDecoration(
+//             border: Border.all(color: TColors.grey),
+//             borderRadius: BorderRadius.circular(100),
+//           ),
+//           child: IconButton(
+//             onPressed: () => Get.to(() => const FillInforSignupGoogle()),
+//             icon: const Image(
+//               width: TSizes.iconMd,
+//               height: TSizes.iconMd,
+//               image: AssetImage(TImages.google),
+//             ),
+//           ),
+//         ),
+//         const SizedBox(width: TSizes.spaceBtwItems),
+//         Container(
+//           decoration: BoxDecoration(
+//             border: Border.all(color: TColors.grey),
+//             borderRadius: BorderRadius.circular(100),
+//           ),
+//           child: IconButton(
+//             onPressed: () {},
+//             icon: const Image(
+//               width: TSizes.iconMd,
+//               height: TSizes.iconMd,
+//               image: AssetImage(TImages.facebook),
+//             ),
+//           ),
+//         ),
+//       ],
+//     );
+//   }
+// }
+// import 'package:flutter/material.dart';
+// import 'package:get/get.dart';
+// import 'package:url_launcher/url_launcher.dart';
+// import 'package:onedaytrip/features/authentication/screens/signup/fill_infor_signup_google.dart';
+//
+// import '../../../utils/constants/colors.dart';
+// import '../../../utils/constants/image_strings.dart';
+// import '../../../utils/constants/sizes.dart';
+//
+// class TSocialButtons extends StatelessWidget {
+//   const TSocialButtons({super.key});
+//
+//   void _launchURL(BuildContext context) async {
+//     const url = "https://trip-by-day-backend.onrender.com/oauth2/authorization/google";
+//     final Uri uri = Uri.parse(url);
+//     if (!await launchUrl(uri)) {
+//       ScaffoldMessenger.of(context).showSnackBar(
+//         SnackBar(content: Text('Could not launch $url')),
+//       );
+//     }
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Row(
+//       mainAxisAlignment: MainAxisAlignment.center,
+//       children: [
+//         Container(
+//           decoration: BoxDecoration(
+//             border: Border.all(color: TColors.grey),
+//             borderRadius: BorderRadius.circular(100),
+//           ),
+//           child: IconButton(
+//             onPressed: () => _launchURL(context),
+//             icon: const Image(
+//               width: TSizes.iconMd,
+//               height: TSizes.iconMd,
+//               image: AssetImage(TImages.google),
+//             ),
+//           ),
+//         ),
+//         const SizedBox(width: TSizes.spaceBtwItems),
+//         Container(
+//           decoration: BoxDecoration(
+//             border: Border.all(color: TColors.grey),
+//             borderRadius: BorderRadius.circular(100),
+//           ),
+//           child: IconButton(
+//             onPressed: () {},
+//             icon: const Image(
+//               width: TSizes.iconMd,
+//               height: TSizes.iconMd,
+//               image: AssetImage(TImages.facebook),
+//             ),
+//           ),
+//         ),
+//       ],
+//     );
+//   }
+// }
+
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'package:onedaytrip/features/authentication/screens/signup/fill_infor_signup_google.dart';
-
+import '../../../api/firebase/global_variables/user_manager.dart';
 import '../../../utils/constants/colors.dart';
 import '../../../utils/constants/image_strings.dart';
 import '../../../utils/constants/sizes.dart';
+import 'dart:convert'; // Thêm import này để sử dụng jsonDecode
 
 class TSocialButtons extends StatelessWidget {
   const TSocialButtons({super.key});
+
+  void _launchURL(BuildContext context) async {
+    const url = "https://trip-by-day-backend.onrender.com/oauth2/authorization/google";
+    final Uri uri = Uri.parse(url);
+    if (await launchUrl(uri)) {
+      // Nếu thành công trong việc mở URL, lắng nghe phản hồi
+      // Bạn có thể sử dụng một package như uni_links hoặc receive_sharing_intent để xử lý URL trở về.
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Could not launch $url')),
+      );
+    }
+  }
+
+  Future<void> _handleCallback(Uri uri) async {
+    // Giả sử callback trả về là: "https://yourapp.com/callback?userId=1&userEmail=test@example.com"
+    final queryParams = uri.queryParameters;
+    final userId = queryParams['userId'];
+    final userEmail = queryParams['userEmail'];
+
+    if (userId != null && userEmail != null) {
+      UserManager().id = int.parse(userId);
+      UserManager().email = userEmail;
+
+      // Chuyển đến trang FillInforSignupGoogle
+      Get.to(() => const FillInforSignupGoogle());
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,7 +161,7 @@ class TSocialButtons extends StatelessWidget {
             borderRadius: BorderRadius.circular(100),
           ),
           child: IconButton(
-            onPressed: () => Get.to(() => const FillInforSignupGoogle()),
+            onPressed: () => _launchURL(context),
             icon: const Image(
               width: TSizes.iconMd,
               height: TSizes.iconMd,
--- lib/features/authentication/screens/signup/widgets/signup_form.dart
+++ lib/features/authentication/screens/signup/widgets/signup_form.dart
@@ -1,253 +1,87 @@
-// import 'package:flutter/material.dart';
-// import 'package:get/get.dart';
-// import 'package:iconsax/iconsax.dart';
-// import 'package:onedaytrip/features/authentication/screens/login/login.dart';
-// import 'package:onedaytrip/features/authentication/screens/signup/verify_email.dart';
-//
-// import '../../../../../utils/constants/sizes.dart';
-// import '../../../../../utils/constants/text_strings.dart';
-//
-// class TSignupForm extends StatelessWidget {
-//   const TSignupForm({
-//     super.key,
-//     required ValueNotifier<String?> selectedGender,
-//   }) : _selectedGender = selectedGender;
-//
-//   final ValueNotifier<String?> _selectedGender;
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Form(
-//       child: Column(
-//         children: [
-//           // Row(
-//           //   children: [
-//           //     Expanded(
-//           //       child: TextFormField(
-//           //         expands: false,
-//           //         decoration: const InputDecoration(
-//           //             labelText: TTexts.firstName,
-//           //             prefixIcon: Icon(Iconsax.user)),
-//           //       ),
-//           //     ),
-//           //     const SizedBox(width: TSizes.spaceBtwInputFields),
-//           //     Expanded(
-//           //       child: TextFormField(
-//           //         expands: false,
-//           //         decoration: const InputDecoration(
-//           //             labelText: TTexts.lastName,
-//           //             prefixIcon: Icon(Iconsax.user)),
-//           //       ),
-//           //     ),
-//           //   ],
-//           // ),
-//           // const SizedBox(height: TSizes.spaceBtwInputFields),
-//
-//           /// Fullname
-//           TextFormField(
-//             expands: false,
-//             decoration: const InputDecoration(
-//                 labelText: TTexts.fullName,
-//                 prefixIcon: Icon(Iconsax.user)),
-//           ),
-//           const SizedBox(height: TSizes.spaceBtwInputFields),
-//
-//           // /// Email
-//           // TextFormField(
-//           //   decoration: const InputDecoration(
-//           //       labelText: TTexts.email, prefixIcon: Icon(Iconsax.direct)),
-//           // ),
-//           // const SizedBox(height: TSizes.spaceBtwInputFields),
-//
-//           /// Phone Number
-//           TextFormField(
-//             decoration: const InputDecoration(
-//                 labelText: TTexts.phoneNo, prefixIcon: Icon(Iconsax.call)),
-//           ),
-//           const SizedBox(height: TSizes.spaceBtwInputFields),
-//
-//           /// Address
-//           TextFormField(
-//             expands: false,
-//             decoration: const InputDecoration(
-//                 labelText: TTexts.address, prefixIcon: Icon(Iconsax.location)),
-//           ),
-//           const SizedBox(height: TSizes.spaceBtwInputFields),
-//
-//           /// Gender Dropdown
-//           ValueListenableBuilder<String?>(
-//             valueListenable: _selectedGender,
-//             builder: (context, value, child) {
-//               return DropdownButtonFormField<String>(
-//                 value: value,
-//                 onChanged: (String? newValue) {
-//                   _selectedGender.value = newValue;
-//                 },
-//                 items: <String>['Male', 'Female', 'Other']
-//                     .map<DropdownMenuItem<String>>((String value) {
-//                   return DropdownMenuItem<String>(
-//                     value: value,
-//                     child: Text(value),
-//                   );
-//                 }).toList(),
-//                 decoration: const InputDecoration(
-//                   labelText: 'Gender',
-//                   prefixIcon: Icon(Iconsax.user),
-//                 ),
-//               );
-//             },
-//           ),
-//           const SizedBox(height: TSizes.spaceBtwInputFields),
-//
-//           /// Password
-//           TextFormField(
-//             obscureText: true,
-//             decoration: const InputDecoration(
-//               labelText: TTexts.password,
-//               prefixIcon: Icon(Iconsax.password_check),
-//               suffixIcon: Icon(Iconsax.eye_slash),
-//             ),
-//           ),
-//           const SizedBox(height: TSizes.spaceBtwInputFields),
-//
-//           /// Confirm Password
-//           TextFormField(
-//             obscureText: true,
-//             decoration: const InputDecoration(
-//               labelText: TTexts.conPassword,
-//               prefixIcon: Icon(Iconsax.password_check),
-//               suffixIcon: Icon(Iconsax.eye_slash),
-//             ),
-//           ),
-//           const SizedBox(height: TSizes.spaceBtwSections),
-//
-//           /// Sign Up Button
-//           SizedBox(
-//             width: double.infinity,
-//             child: ElevatedButton(
-//                 onPressed: () => Get.to(() => const LoginScreen()), child: const Text(TTexts.createAccount)),
-//           )
-//         ],
-//       ),
-//     );
-//   }
-// }
-
-
-import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
-import 'package:http/http.dart' as http;
 import 'package:onedaytrip/features/authentication/screens/login/login.dart';
+import 'package:onedaytrip/features/authentication/screens/signup/verify_email.dart';
+
 import '../../../../../utils/constants/sizes.dart';
 import '../../../../../utils/constants/text_strings.dart';
 
-class TSignupForm extends StatefulWidget {
-  final int userId;
-  final String userEmail;
-
+class TSignupForm extends StatelessWidget {
   const TSignupForm({
     super.key,
-    required this.userId,
-    required this.userEmail,
     required ValueNotifier<String?> selectedGender,
   }) : _selectedGender = selectedGender;
 
   final ValueNotifier<String?> _selectedGender;
 
   @override
-  _TSignupFormState createState() => _TSignupFormState();
-}
-
-class _TSignupFormState extends State<TSignupForm> {
-  final TextEditingController _fullnameController = TextEditingController();
-  final TextEditingController _phoneController = TextEditingController();
-  final TextEditingController _passwordController = TextEditingController();
-  final TextEditingController _confirmPasswordController = TextEditingController();
-  final TextEditingController _addressController = TextEditingController();
-
-  Future<void> saveUserInfo() async {
-    final url = 'https://trip-by-day-backend.onrender.com/api/v1/auth/save-infor';
-    final headers = {
-      'Content-Type': 'application/json',
-      'Accept': 'application/json',
-    };
-    final body = jsonEncode({
-      'email': widget.userEmail,
-      'fullname': _fullnameController.text,
-      'phone': _phoneController.text,
-      'password': _passwordController.text,
-      'address': _addressController.text,
-      'gender': widget._selectedGender.value?.toUpperCase(), // Chuyển đổi thành chữ hoa
-    });
-
-    try {
-      final response = await http.post(
-        Uri.parse(url),
-        headers: headers,
-        body: body,
-      );
-
-      print('Status code: ${response.statusCode}');
-      print('Response body: ${response.body}');
-
-      if (response.statusCode == 201) {
-        print('User information saved successfully');
-        Get.to(() => const LoginScreen());
-      } else {
-        print('Failed to save user information');
-        Get.snackbar('Error', 'Failed to save user information: ${response.statusCode}');
-      }
-    } catch (error) {
-      print('Error: $error');
-      Get.snackbar('Error', 'An error occurred: $error');
-    }
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Form(
       child: Column(
         children: [
-          // Fullname
+          // Row(
+          //   children: [
+          //     Expanded(
+          //       child: TextFormField(
+          //         expands: false,
+          //         decoration: const InputDecoration(
+          //             labelText: TTexts.firstName,
+          //             prefixIcon: Icon(Iconsax.user)),
+          //       ),
+          //     ),
+          //     const SizedBox(width: TSizes.spaceBtwInputFields),
+          //     Expanded(
+          //       child: TextFormField(
+          //         expands: false,
+          //         decoration: const InputDecoration(
+          //             labelText: TTexts.lastName,
+          //             prefixIcon: Icon(Iconsax.user)),
+          //       ),
+          //     ),
+          //   ],
+          // ),
+          // const SizedBox(height: TSizes.spaceBtwInputFields),
+
+          /// Fullname
           TextFormField(
-            controller: _fullnameController,
+            expands: false,
             decoration: const InputDecoration(
-              labelText: TTexts.fullName,
-              prefixIcon: Icon(Iconsax.user),
-            ),
+                labelText: TTexts.fullName,
+                prefixIcon: Icon(Iconsax.user)),
           ),
           const SizedBox(height: TSizes.spaceBtwInputFields),
 
-          // Phone Number
+          // /// Email
+          // TextFormField(
+          //   decoration: const InputDecoration(
+          //       labelText: TTexts.email, prefixIcon: Icon(Iconsax.direct)),
+          // ),
+          // const SizedBox(height: TSizes.spaceBtwInputFields),
+
+          /// Phone Number
           TextFormField(
-            controller: _phoneController,
             decoration: const InputDecoration(
-              labelText: TTexts.phoneNo,
-              prefixIcon: Icon(Iconsax.call),
-            ),
+                labelText: TTexts.phoneNo, prefixIcon: Icon(Iconsax.call)),
           ),
           const SizedBox(height: TSizes.spaceBtwInputFields),
 
-          // Address
+          /// Address
           TextFormField(
-            controller: _addressController,
+            expands: false,
             decoration: const InputDecoration(
-              labelText: TTexts.address,
-              prefixIcon: Icon(Iconsax.location),
-            ),
+                labelText: TTexts.address, prefixIcon: Icon(Iconsax.location)),
           ),
           const SizedBox(height: TSizes.spaceBtwInputFields),
 
-          // Gender Dropdown
+          /// Gender Dropdown
           ValueListenableBuilder<String?>(
-            valueListenable: widget._selectedGender,
+            valueListenable: _selectedGender,
             builder: (context, value, child) {
               return DropdownButtonFormField<String>(
                 value: value,
                 onChanged: (String? newValue) {
-                  widget._selectedGender.value = newValue;
+                  _selectedGender.value = newValue;
                 },
                 items: <String>['Male', 'Female', 'Other']
                     .map<DropdownMenuItem<String>>((String value) {
@@ -265,9 +99,8 @@ class _TSignupFormState extends State<TSignupForm> {
           ),
           const SizedBox(height: TSizes.spaceBtwInputFields),
 
-          // Password
+          /// Password
           TextFormField(
-            controller: _passwordController,
             obscureText: true,
             decoration: const InputDecoration(
               labelText: TTexts.password,
@@ -277,9 +110,8 @@ class _TSignupFormState extends State<TSignupForm> {
           ),
           const SizedBox(height: TSizes.spaceBtwInputFields),
 
-          // Confirm Password
+          /// Confirm Password
           TextFormField(
-            controller: _confirmPasswordController,
             obscureText: true,
             decoration: const InputDecoration(
               labelText: TTexts.conPassword,
@@ -289,19 +121,11 @@ class _TSignupFormState extends State<TSignupForm> {
           ),
           const SizedBox(height: TSizes.spaceBtwSections),
 
-          // Sign Up Button
+          /// Sign Up Button
           SizedBox(
             width: double.infinity,
             child: ElevatedButton(
-              onPressed: () {
-                if (_passwordController.text == _confirmPasswordController.text) {
-                  saveUserInfo();
-                } else {
-                  Get.snackbar('Error', 'Passwords do not match');
-                }
-              },
-              child: const Text(TTexts.createAccount),
-            ),
+                onPressed: () => Get.to(() => const LoginScreen()), child: const Text(TTexts.createAccount)),
           )
         ],
       ),
